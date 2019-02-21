0beb2c6d3a07b7a78a539531a00a99b2df61099c
David Kezi <davidkezi6@gmail.com>
2019-02-21 16:17:39 +0100

    feat: add basic logic

---
+++ main.go
@@ -0,0 +1,67 @@
+package atlas
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const baseURL string = "http://localhost:3030/v1/sms/send_sms"
+
+// Client is a representation of a person
+type Client struct {
+	Username string
+	Password string
+}
+
+// NewBasicAuthClient is a representation of a person
+func NewBasicAuthClient(username, password string) *Client {
+	return &Client{
+		Username: username,
+		Password: password,
+	}
+}
+
+// Sms is the sms structure
+type Sms struct {
+	To      string `json:"to"`
+	Sender  string `json:"sender"`
+	Content string `json:"content"`
+}
+
+// SendSMS is to send the SMS
+func (s *Client) SendSMS(sms *Sms) error {
+	url := fmt.Sprintf(baseURL, s.Username)
+	fmt.Println(url)
+	j, err := json.Marshal(sms)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(j))
+	if err != nil {
+		return err
+	}
+	_, err = s.doRequest(req)
+	return err
+}
+
+// doRequest actually does the sendin
+func (s *Client) doRequest(req *http.Request) ([]byte, error) {
+	req.SetBasicAuth(s.Username, s.Password)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if 200 != resp.StatusCode {
+		return nil, fmt.Errorf("%s", body)
+	}
+	return body, nil
+}
